62246480829502c11ba9a48f9755498043595b0a
mahparaamil <mahparaamil96@gmail.com>
2026-02-18 13:56:16 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        This is my test i need to deploy this app to vercel
+        </p>Testing for vercel deployment
         <p></p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
