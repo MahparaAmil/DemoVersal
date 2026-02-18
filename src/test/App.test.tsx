a57a4c800c8292dc1eaa19b400f478c42c98f040
mahparaamil <mahparaamil96@gmail.com>
2026-02-18 19:22:14 +0100

    Deployimg after CI fixes

---
+++ src/test/App.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+
+});
+
+test('switch text when i click on it', () => {
+  render(<App />);
+ const websiteLink = screen.getByText(/learn EPIECH/i);
+ expect(websiteLink).toHaveAttribute('href', 'https://my.epitech.eu/');  
+});
