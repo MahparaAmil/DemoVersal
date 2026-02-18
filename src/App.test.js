38ad6fdda5a4772ad2d9c71ff626755258e036d0
mahparaamil <mahparaamil96@gmail.com>
2026-02-18 16:09:55 +0100

    Testing with one fail and one pass test

--- src/App.test.js
+++ src/App.test.js
@@ -5,4 +5,11 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+});
+
+test('switch text when i click on it', () => {
+  render(<App />);
+ const websiteLink = screen.getByText(/learn EPIECH/i);
+ expect(websiteLink).toHaveAttribute('href', 'https://my.epitech.eu/');  
 });
